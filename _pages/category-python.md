b6389791f7edab6d816a86c601bc5494f3dada30
yoonseok0323 <yoonrock02@gamil.com>
2022-06-05 16:31:11 +0900

    test

--- _pages/category-python.md
+++ _pages/category-python.md
@@ -7,5 +7,3 @@ sidebar_main: true
 ---
 
 
-{% assign posts = site.categories.python %}
-{% for post in posts %} {% include archive-single.html type=page.entries_layout %} {% endfor %}
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Practice",
+        "excerpt":"“여기가 본문내용”   ","categories": ["Blog"],
+        "tags": ["Blog","Git"],
+        "url": "/blog/First/",
+        "teaser": null
+      }]
